a083bd76b60e195071c236d70f9d68f750a51045
Sahil Saxena <sahilsaxena9711@gmail.com>
2018-07-31 00:06:54 +0530

    added getEmployeeById() from database and fixed version issue with spring-boot

--- src/main/java/org/api/Dao/RealEmployeeDaoIml.java
+++ src/main/java/org/api/Dao/RealEmployeeDaoIml.java
@@ -16,25 +16,30 @@ public class RealEmployeeDaoIml implements EmployeeDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private static class EmployeeRowMapper implements RowMapper<Employee>{
+
+        @Override
+        public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
+            Employee employee = new Employee();
+            employee.setId(resultSet.getInt("id"));
+            employee.setName(resultSet.getString("name"));
+            employee.setDesignation(resultSet.getString("designation"));
+            return employee;
+        }
+    }
+
     @Override
     public Collection<Employee> getAllEmployee() {
-        final String sql = "SELECT id, name, designation FROM employees";
-        List<Employee> employee = jdbcTemplate.query(sql, new RowMapper<Employee>() {
-            @Override
-            public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
-                Employee employee = new Employee();
-                employee.setId(resultSet.getInt("id"));
-                employee.setName(resultSet.getString("name"));
-                employee.setDesignation(resultSet.getString("designation"));
+        final String sql = "SELECT id, name, designation FROM employee";
+        List<Employee> employee = jdbcTemplate.query(sql,new EmployeeRowMapper());
                 return employee;
-            }
-        });
-        return employee;
     }
 
     @Override
     public Employee getEmloyeeById(int id) {
-        return null;
+        final String sql = "SELECT id, name, designation FROM employee where id = ?";
+        Employee employee = jdbcTemplate.queryForObject(sql, new EmployeeRowMapper(), id);
+        return employee;
     }
 
     @Override
